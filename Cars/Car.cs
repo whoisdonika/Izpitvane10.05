5f0926a60d50830f43003e2c7f43111c9c9c735d
STUDENTS-1212-4 <desislava_tsocheva@abv.bg>
2023-05-10 11:41:30 +0300

    Add project files.

---
+++ Cars/Car.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    internal class Car
+    {
+		private string brand;
+
+		public string Brand
+		{
+			get { return brand; }
+			set { brand = value; }
+		}
+
+		private string model;
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		private string number;
+
+		public string Number
+		{
+			get { return number; }
+			set { number = value; }
+		}
+
+		private int year;
+
+		public int Year
+		{
+			get { return year; }
+			set 
+			{
+				if (value > 1768)
+				{
+                   year = value;
+					
+				
+				}
+				else
+				{
+					Console.Write("Invalid value for the car's year. Try again!");
+					year = 0;
+				}
+				
+			}
+		}
+
+		public void EcoOrNot()
+		{
+            if (this.Year > 2000)
+            {
+                Console.WriteLine($"{this.Brand} {this.Model} : I am an ECO car :)");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Brand} {this.Model} : I am not an ECO car :(");
+            }
+        }
+		public void DriveCar()
+		{
+			Console.WriteLine($"{this.Brand} {this.Model} : Brum-brum-brum!");
+		}
+
+		public void PrintMe()
+		{
+			Console.WriteLine($"Brand : {this.Brand}, Model : {this.Model}, Number : {this.Number} Year : {this.Year}");
+		}
+       
+		private static int counter;
+        public static int Counter
+        {
+			get { return counter;}
+        }
+
+		//public Car(string brand, string model, string number, int year)
+		//{
+		//	this.brand = brand;
+		//	this.model = model;
+		//	this.number = number;
+		//	this.year = year;
+
+		//	counter++;
+
+
+		//}
+
+		
+
+
+
+
+
+
+	}
+}
---
+++ Cars/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            Console.Write("Do you want to add any cars? - ");
+            string add = Console.ReadLine();
+
+                while (add == "yes")
+                {
+                    Car car = new Car();
+                    Console.Write("What's the car's brand? - ");
+                    string brand = Console.ReadLine();
+                    Console.Write("What's the car's model? - ");
+                    string model = Console.ReadLine();
+                    Console.Write("What's the car's licence plate? - ");
+                    string licencePlate = Console.ReadLine();
+                    Console.Write("What's the car's year of fabrication? - ");
+                    int year = int.Parse(Console.ReadLine());
+
+                    car.Brand = brand;
+                    car.Model = model;
+                    car.Number = licencePlate;
+                    car.Year = year;
+
+                    car.PrintMe();
+                    car.EcoOrNot();
+                    car.DriveCar();
+                     
+                    cars.Add(car);
+
+                    Console.Write("Do you want to add any more cars? - ");
+                    add = Console.ReadLine();
+                }
+               if (add == "no")
+               {
+                Console.WriteLine($"There are {cars.Count} racers.");
+                return;
+               }
+
+
+
+
+            //Car car1 = new Car("Opel", "Astra", "PB3452AR", 2003);
+            //Car car2 = new Car("Wolkswagen", "Golf", "SV7698CD", 2011);
+            //Car car3 = new Car("Mercedes", "Benz", "NH3865DH", 2013);
+
+            //car1.PrintMe();
+            //car2.PrintMe();
+            //car3.PrintMe();
+
+            //Console.WriteLine();
+
+            //car1.EcoOrNot();
+            //car2.EcoOrNot();
+            //car3.EcoOrNot();
+
+            //Console.WriteLine();
+
+            //car1.DriveCar();
+            //car2.DriveCar();
+            //car3.DriveCar();
+
+            //Console.WriteLine();
+
+
+
+
+        }
+    }
+}
